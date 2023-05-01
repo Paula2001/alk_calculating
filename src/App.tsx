68c7995a1cc54feae82b426f1f3f547d3fdcff03
Paula <paula@pop-os.localdomain>
2023-05-01 12:10:44 +0200

    added some edits

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import BACCal from "./BacCal.tsx";
 import './App.css'
-import { Form, Button, Container, Row, Col } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
+import {Paper} from "@mui/material";
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,8 @@ function App() {
   return (
     <Container>
         <BACCal />
+        <Paper style={{margin: 10, fontWeight: "bolder"}}>⚠️Caution: non of the numbers are super accurate please seek medical assistance in case of medical emergency ⚠️</Paper>
+        <Paper style={{margin: 10, fontWeight: "bold"}}>Made with ⚛️ And ❤️ By <a href={"https://paula-george.guru"} >Paula Gawargious</a> And remember don't 🍺 and 🚗 </Paper>
     </Container>
   )
 }
